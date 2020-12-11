4b5ccfa8923efe27a00b465a33e4c8892fec36f6
Dvir <dd2640@bs.amalnet.k12.il>
2020-12-11 18:55:24 +0200

    Update

--- app/src/main/java/com/example/alpha_version/Signup.java
+++ app/src/main/java/com/example/alpha_version/Signup.java
@@ -19,11 +19,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Signup extends AppCompatActivity implements View.OnClickListener {
+public class Signup extends AppCompatActivity {
 
-    EditText ETName,ETMail,ETPass;
+    EditText ETName, ETMail, ETPass;
     private FirebaseAuth fAuth;
-    Button SignUp,back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,87 +32,79 @@ public class Signup extends AppCompatActivity implements View.OnClickListener {
         ETPass = (EditText) findViewById(R.id.ETPass);
         ETName = (EditText) findViewById(R.id.ETName);
         fAuth = FirebaseAuth.getInstance();
-        SignUp = (Button) findViewById(R.id.SignUp);
-        SignUp.setOnClickListener(this);
-        back = (Button) findViewById(R.id.back);
-        back.setOnClickListener(this);
     }
 
     /**
      * @since 1/11/2020
-     * @param v
      */
-    @Override
-    public void onClick(View v) {
-        Intent si = new Intent (this, MainActivity.class);
-        switch (v.getId()){
-            case R.id.back:
-                finish();
-                break;
-            case R.id.SignUp:
-                registerUser();
-                break;
-        }
+
+    public void SignUp(View view) {
+        registerUser();
     }
 
-    private void registerUser(){
+    public void Back(View view) {
+        finish();
+    }
+
+    private void registerUser() {
         String email = ETMail.getText().toString();
         String Name = ETName.getText().toString();
         String password = ETPass.getText().toString();
-        if(Name.isEmpty()){
+        if (Name.isEmpty()) {
             ETName.setError("Full name is required");
             ETName.requestFocus();
             return;
         }
-        if(email.isEmpty()){
+        if (email.isEmpty()) {
             ETMail.setError("Email is required");
             ETMail.requestFocus();
             return;
         }
-        if(password.isEmpty()){
+        if (password.isEmpty()) {
             ETPass.setError("Password is required");
             ETPass.requestFocus();
             return;
         }
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             ETMail.setError("Please provide valid email");
             ETMail.requestFocus();
             return;
         }
 
-        if(password.length()<6){
+        if (password.length() < 6) {
             ETPass.setError("Password is should be 6 Characters!");
             ETPass.requestFocus();
             return;
         }
 
-        fAuth.createUserWithEmailAndPassword(email,password)
+        fAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Users user = new Users(Name , email);
+                        if (task.isSuccessful()) {
+                            Users user = new Users(Name, email);
                             FirebaseDatabase.getInstance().getReference("Users")
                                     .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
                                     .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful()){
+                                    if (task.isSuccessful()) {
                                         Toast.makeText(Signup.this, "User has been registered successfully!", Toast.LENGTH_SHORT).show();
                                         //Move the user to the login activity
-                                    }
-                                    else {
+                                    } else {
                                         Toast.makeText(Signup.this, "Failed to sign up", Toast.LENGTH_SHORT).show();
 
                                     }
                                 }
                             });
-                        }
-                        else{
+                        } else {
                             Toast.makeText(Signup.this, "Failed to sign up", Toast.LENGTH_SHORT).show();
                         }
 
                     }
                 });
     }
+
+
 }
+
